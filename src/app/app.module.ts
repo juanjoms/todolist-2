3674c17cf6ef0b68c11fc846c9a5642e886504a0
Juan Miramontes <juan.miramontes@accenture.com>
2019-05-28 01:30:19 -0500

    Added missing folders

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { TodoListComponent } from './todo-list/todo-list.component';
+import { TodoDetailComponent } from './todo-detail/todo-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoDetailComponent,
+    TodoListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/models/to-do.ts
@@ -0,0 +1,5 @@
+export class ToDo {
+  title: string;
+  isCompleted: boolean;
+  subtasks: string[];
+}
